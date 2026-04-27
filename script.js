07c16eaa3cb852065af4ed9e04822fe3a8cb7163
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-04-27 11:07:14 -0300

    add - correção

--- script.js
+++ script.js
@@ -17,26 +17,15 @@ document.getElementById("form-login").onsubmit = (e) => {
     let mensagem = document.getElementById("mensagem");
     mensagem.innerHTML = "";
     if (!email.includes("@") || !email.includes(".")) {
-        mensagem.innerHTML = "<div class='erro'><p> Email Invalido !< /p></div>";
+        mensagem.innerHTML = "<div class='erro'><p> Email Invalido !</p></div>";
         return;
 
     }
     if (senha.length < 4) {
-        mensagem.innerHTML = "<div class='erro'><p> Senha muito curta !< /p></div>";
+        mensagem.innerHTML = "<div class='erro'><p> Senha muito curta !</p></div>";
         return;
     }
 
-    if (cadastro) {
-        localStorage.setItem(email, senha);
-        mensagem.innerHTML = "<div class='sucesso'><p> Cadastrado com sucesso !< /p></div>";
-    } else {
-        let salva = localStorage.getItem(email);
-        if (salva === senha) {
-            mensagem.innerHTML = "<div class='sucesso'><p>Login com sucesso !< /p></div>";
-        } else {
-            mensagem.innerHTML = "<div class='erro'><p>Dados Incorretos !< /p></div>";
-        }
-
-    }
+    
     document.getElementById("form-login").reset();
 }
